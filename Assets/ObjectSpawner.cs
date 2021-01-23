b676932b0cf77fa0cc1adf5436084d235b0e0563
eVoo <superyassine15@gmail.com>
2021-01-23 18:26:34 +0100

    init

---
+++ Assets/ObjectSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpawner : MonoBehaviour
+{
+    public GameObject objectToSpawn;
+
+    private TapToPlaceIndicator TapToPlaceIndicator;
+
+
+    void Start()
+    {
+        TapToPlaceIndicator = FindObjectOfType<TapToPlaceIndicator>();
+
+    }
+
+    void Update()
+    {
+        if (Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            GameObject obj = Instantiate(objectToSpawn,
+                TapToPlaceIndicator.transform.position, TapToPlaceIndicator.transform.rotation);
+
+        }
+
+    }
+
+    public void Activate()
+    {
+
+        GameObject obj = Instantiate(objectToSpawn,
+                TapToPlaceIndicator.transform.position, TapToPlaceIndicator.transform.rotation);
+
+    }
+
+
+}
